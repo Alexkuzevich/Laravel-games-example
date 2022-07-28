d67eebd1e5262f5c2aa8319513c125065b12d2bc
Alexander Kuzevich <enaken0212@gmail.com>
2022-07-28 21:16:16 +0300

    Initial commit

---
+++ database/migrations/2022_07_25_191155_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('slug')->comment('product url');
+            $table->string('preview_image')->comment('product image');
+            $table->integer('position')->default(1000);
+            $table->string('video')->comment('product video-review url');
+            $table->boolean('novelty');
+            $table->integer('status')->default(10);
+
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
